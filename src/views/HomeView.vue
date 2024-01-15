c810c06df5929f06358fcef91bcf2aae1fb0d78c
henrietadapkute <eimante.dapkute@gmail.com>
2024-01-15 09:10:29 +0000

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Travel Planner</h1>
+    <h3>Welcome to my app, here you can add all of your future travel destination plans</h3>
+  </main>
+</template>
